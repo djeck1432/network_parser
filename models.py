db37ae840d3869265bffa1ddd0b734e6f0aac6d3
Oleh Kostashchuk <oleh.kostashchuk@creativedock.com>
2023-06-07 21:36:26 +0200

    add new model method get_id_by_name

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from typing import Any, Optional
 
 from pydantic import BaseModel
-from sqlalchemy import JSON, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import JSON, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm.session import Session
 
 Base = declarative_base()
 
@@ -16,13 +17,24 @@ class NetworkConfigModel(Base):
 
     id = Column(Integer, primary_key=True)
     connection = Column(Integer)
-    name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False, index=True)
     description = Column(String(255))
     config = Column(JSON)
     type = Column(String(50))
     infra_type = Column(String(50))
     port_channel_id = Column(Integer)
     max_frame_size = Column(Integer)
+
+    @classmethod
+    def get_id_by_name(cls, session: Session, name: str) -> int | None:
+        """
+        This method returns the ID of the entry by its name.
+        :param session: db session
+        :param name: name of the entry
+        :return: None or int
+        """
+        instance = session.query(cls).filter(cls.name==name).first()
+        return instance.id if instance else None
 
 
 class NetworkConfigPydanticModel(BaseModel):
